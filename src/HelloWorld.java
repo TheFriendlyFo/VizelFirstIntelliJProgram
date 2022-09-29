a6aa3a22f7addf2ba80169636b5c913a4d9e4371
SethV <seth.vizel@gmail.com>
2022-09-28 21:43:06 -0400

    Still just messing around.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hellooooo World!");
+        String disp = "";
+        for (int i=0; i<10000; i++) {
+            if (i % 100 == 0) {
+                disp = disp.concat("\n");
+            }
+            disp = disp.concat(i % 9 + " ");
+        }
+
+        System.out.println(disp);
     }
 }
